3880550d1cc612d944aac58479705eb1bc184d31
Rezwan Al Kaoser <rezwan2525@gmail.com>
2025-06-27 12:11:09 +0600

    added cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 import gsap from 'gsap';
 import Navbar from './components/navbar';
 import Hero from './components/Hero';
+import Art from './components/Art';
+import Cocktail from './components/Cocktail';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +13,8 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <Cocktail />
+      <Art />
     </main>
   )
 }
